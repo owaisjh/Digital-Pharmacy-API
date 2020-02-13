a75f74a33998ab184e5d3e44460f508e0bf7d5c1
Owais Hetavkar <owaisjh12@gmail.com>
2020-02-14 01:23:08 +0530

    10th

--- app.py
+++ app.py
@@ -9,35 +9,35 @@ from delivery import Orders, Complete
 
 from datetime import timedelta
 
-if __name__ == "__main__":
-    app = Flask(__name__)
-    app.secret_key = 'safety'
-    api = Api(app)
 
-    app.config['JWT_AUTH_URL_RULE'] = '/login'
-    app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=99999999999)
+app = Flask(__name__)
+app.secret_key = 'safety'
+api = Api(app)
 
-
-    jwt = JWT(app, authenticate, identity)    #authentication
+app.config['JWT_AUTH_URL_RULE'] = '/login'
+app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=99999999999)
 
 
-    api.add_resource(UserRegister, '/sign_up')
-    api.add_resource(SetProfile, '/set_profile')
-    api.add_resource(GetProfile, '/get_profile')
-    api.add_resource(AllMeds, '/all_meds')
-    api.add_resource(CatCall, '/cat_call')
-    api.add_resource(Search, '/search')
-    api.add_resource(AddOrder, '/add_order')
-    api.add_resource(Update, '/update')
+jwt = JWT(app, authenticate, identity)    #authentication
 
 
-    api.add_resource(Orders, '/orders')
-    api.add_resource(Complete, '/complete')
+api.add_resource(UserRegister, '/sign_up')
+api.add_resource(SetProfile, '/set_profile')
+api.add_resource(GetProfile, '/get_profile')
+api.add_resource(AllMeds, '/all_meds')
+api.add_resource(CatCall, '/cat_call')
+api.add_resource(Search, '/search')
+api.add_resource(AddOrder, '/add_order')
+api.add_resource(Update, '/update')
+
+
+api.add_resource(Orders, '/orders')
+api.add_resource(Complete, '/complete')
 
 
 
 
-    app.run(port=5000, debug=True)
+app.run(port=5000, debug=True)
 
 
 
