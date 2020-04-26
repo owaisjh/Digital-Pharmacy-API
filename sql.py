15477bef1185024f2a61fdfad4219206e5b66c32
Owais Hetavkar <owaisjh12@gmail.com>
2020-04-27 03:59:29 +0530

    removed meds

--- sql.py
+++ sql.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-connection = sqlite3.connect('order.db')
+connection = sqlite3.connect('meds.db')
 
 cursor = connection.cursor()
 
@@ -8,10 +8,10 @@ cursor = connection.cursor()
 
 #create_table = "ALTER TABLE allmeds ADD PRIMARY KEY (id) "
 
-#create_table = "DELETE FROM allmeds WHERE category="
+create_table = "DELETE FROM allmeds WHERE category='temp' "
 
 
-create_table = "CREATE TABLE orders (id INTEGER PRIMARY KEY, username text, user_name text, items text, cost int, address text, contact int, paymenttype text, status int, daterec date, datecomp date)"
+#create_table = "CREATE TABLE orders (id INTEGER PRIMARY KEY, username text, user_name text, items text, cost int, address text, contact int, paymenttype text, status int, daterec date, datecomp date)"
 
 cursor.execute(create_table)
 
