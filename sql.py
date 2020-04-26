4469c6b608a92f80b8654346a7b389a78310692f
Owais Hetavkar <owaisjh12@gmail.com>
2020-04-27 03:55:25 +0530

    decreased pres

--- sql.py
+++ sql.py
@@ -1,12 +1,17 @@
 import sqlite3
 
-connection = sqlite3.connect('meds.db')
+connection = sqlite3.connect('order.db')
 
 cursor = connection.cursor()
 
 #create_table = "CREATE TABLE IF NOT EXISTS orders (id INTEGER PRIMARY KEY, username text, user_name text, items text, cost int, address text, contact int, paymenttype text, status int, daterec date, datecomp date) "
 
-create_table = "ALTER TABLE allmeds ADD PRIMARY KEY (id) "
+#create_table = "ALTER TABLE allmeds ADD PRIMARY KEY (id) "
+
+#create_table = "DELETE FROM allmeds WHERE category="
+
+
+create_table = "CREATE TABLE orders (id INTEGER PRIMARY KEY, username text, user_name text, items text, cost int, address text, contact int, paymenttype text, status int, daterec date, datecomp date)"
 
 cursor.execute(create_table)
 
